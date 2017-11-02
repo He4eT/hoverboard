035235db11df9552455c979e0032380f40fd6e97
He4eT <He4eTHb1u@gmail.com>
2017-11-02 07:49:49 +0500

    Sceffold playground

--- src/playground/playground.js
+++ src/playground/playground.js
@@ -4,6 +4,26 @@ console.log('playground')
 
 let peer = new Peer('playground', {key: '4cmlwdtxffphw7b9'})
 
+let ball = document.getElementById('ball')
+
+let x = 500
+let v = 0
+var a = 0
+
+let phys = () => {
+  v *= 0.8
+  v += a * 0.8
+  x += v
+
+  ball.style.left = x + 'px'
+  console.log(a, v, x)
+}
+
 peer.on('connection', conn => {
-  conn.on('data', console.log)
+  conn.on('data', ({y}) => {
+    console.log(y)
+    a = y || 0
+  })
 })
+
+setInterval(phys, 50)
