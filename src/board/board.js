b5e6f4fd07d01c8a1d0d4807170c593ad3caeb28
He4eT <He4eTHb1u@gmail.com>
2017-12-10 04:55:17 +0500

    cleanup

--- src/board/board.js
+++ src/board/board.js
@@ -1,22 +1,35 @@
-import {startSensors, drawPoint} from './sensors'
 import Peer from 'peerjs'
+import NoSleep from 'nosleep.js'
+
+import {startSensors, drawPoint} from './sensors'
 
 console.log('board')
 
 let peer = new Peer('board', {key: '4cmlwdtxffphw7b9'})
-let conn = peer.connect('playground')
+// let connection
 
-conn.on('open', () => {
-  conn.send('hi!')
-  onConnected(conn)
-})
+window.onload = () => {
+  // let startButton = document.querySelector('.start')
+  // startButton.addEventListener('click', () => {
+    let connection = peer.connect('playground')
 
-let onConnected = (conn) => {
+    connection.on('open', () => {
+      connection.send('hi!')
+      onConnected(connection)
+    })
+  // })
+}
+
+let onConnected = (connection) => {
+  let noSleep = new NoSleep()
+  noSleep.enable()
+  alert(3)
+
   let container = document.querySelector('.container')
   let point = document.querySelector('.point')
 
   startSensors((x, y) => {
-    conn.send({y})
+    connection.send({y})
     drawPoint(container, point, x, y)
   })
 }
